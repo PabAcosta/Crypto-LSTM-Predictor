809086883f5b2b0810786c1c6701969bb5883893
Pablo Acosta <aacostapp1@gmail.com>
2026-04-24 16:06:20 -0400

    Added load_crypto_csv function + Added merge_crypto_data function

--- data_handling.py
+++ data_handling.py
@@ -1,9 +1,9 @@
 import os
+import pandas as pd
 from datetime import datetime
 import yfinance
 
 def fetch_crypto_data(tickers, start_date="2018-01-01", end_date=None, save_dir="data"):
-
   # Set the date range for historical data
   if end_date is None:
     end_date = datetime.now().strftime("%Y-%m-%d")
@@ -23,8 +23,38 @@ def fetch_crypto_data(tickers, start_date="2018-01-01", end_date=None, save_dir=
     data.to_csv(file_path)
     print(ticker + " downloaded")
 
+def load_crypto_csv(path, asset_name):
+  # Load the CSV file into a DataFrame
+  df = pd.read_csv(path, skiprows=2, usecols=[0, 1])
+
+  # Rename the columns to 'Date' and Close
+  df.columns = ['Date', asset_name + "_Close"]
+  return df
+
+def merge_crypto_data(asset_files):
+  # Initialize an empty list to hold the DataFrames
+  dataframes = []
+
+  # Loop through the asset files and load each one into a DataFrame
+  for asset_name, path in asset_files.items():
+    df = load_crypto_csv(path, asset_name)
+    dataframes.append(df)
+
+  # Merge all DataFrames on the 'Date' column
+  merged_df = dataframes[0]
+  for df in dataframes[1:]:
+    merged_df = pd.merge(merged_df, df, on='Date')
+  return merged_df
 
 if __name__ == "__main__":
     # Define the cryptocurrency tickers to download
     tickers = ["ETH-USD", "BTC-USD", "XRP-USD", "SOL-USD"]
+    asset_files = {
+    "BTC": "data/BTC_USD.csv",
+    "ETH": "data/ETH_USD.csv",
+    "XRP": "data/XRP_USD.csv",
+    "SOL": "data/SOL_USD.csv"
+    }
+
     fetch_crypto_data(tickers)
+    merge_crypto_data(asset_files)
